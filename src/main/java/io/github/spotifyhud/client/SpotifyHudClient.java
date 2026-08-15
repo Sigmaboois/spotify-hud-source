4a2267c6660a03b155178a4b9853aa2fb636a381
arenzya <elshafeigasser@gmail.com>
2026-08-15 22:39:58 +0300

    Port HUD registration to Fabric 26.2 API

--- src/main/java/io/github/spotifyhud/client/SpotifyHudClient.java
+++ src/main/java/io/github/spotifyhud/client/SpotifyHudClient.java
@@ -2,17 +2,26 @@ package io.github.spotifyhud.client;
 
 import io.github.spotifyhud.client.media.MediaService;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
+import net.minecraft.resources.Identifier;
 
 public final class SpotifyHudClient implements ClientModInitializer {
+    private static final String MOD_ID = "spotifyhud";
     private MediaService media;
 
     @Override
     public void onInitializeClient() {
         media = new MediaService();
         media.start();
+
         HudRenderer renderer = new HudRenderer(media);
-        HudRenderCallback.EVENT.register((context, tickCounter) -> renderer.render(context));
+        HudElementRegistry.attachElementBefore(
+                VanillaHudElements.CHAT,
+                Identifier.fromNamespaceAndPath(MOD_ID, "now_playing"),
+                (graphics, tickCounter) -> renderer.render(graphics)
+        );
+
         Runtime.getRuntime().addShutdownHook(new Thread(media::close, "Spotify HUD shutdown"));
     }
 }
